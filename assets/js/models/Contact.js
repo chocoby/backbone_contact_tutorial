3b0c2b3be0d0daf5e1a70352e73275daddc50cba
chocoby <chocoby@gmail.com>
2013-05-16 15:20:19 +0900

    P178: Contact モデルに並び替えヘルパー/エスケープを追加

--- assets/js/models/Contact.js
+++ assets/js/models/Contact.js
@@ -1,6 +1,19 @@
 define([
   'backbone'
 ], function (Backbone) {
-  var Contact = Backbone.Model.extend({});
+  var Contact = Backbone.Model.extend({
+    index: function() {
+      // 名前で並べ替える
+      // 大文字と小文字は区別しない
+      return this.get('name').toUpperCase();
+    },
+    toEscapedJSON: function() {
+      // HTML エスケープされた属性オブジェクトを返す
+      return _.object(this.map(function (value, attr) {
+        return [attr, _.escape(value)];
+      }));
+    }
+  });
+
   return Contact;
 });
