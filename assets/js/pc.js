532e949ba291d0cffb6757f7b5fe14f00d2e43e4
chocoby <chocoby@gmail.com>
2013-05-17 15:05:28 +0900

    P182: pc.js を実装

---
+++ assets/js/pc.js
@@ -0,0 +1,20 @@
+require([
+  'jquery',
+  'collections/ContactList',
+  'views/pc/AppView',
+  'routers/pc'
+], function($, ContactList, AppView, Router) {
+  var router = new Router();
+  var contactlist new ContactList();
+  contactlist.fetch();
+
+  var appview = new AppView({
+    router: router,
+    collection: contactlist
+  });
+
+  $(function() {
+    $('body').append(appview.render().el);
+    Backbone.history.start();
+  });
+});
