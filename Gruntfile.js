8e66a50a4e572a404e512cd490a490a566d01299
chocoby <chocoby@gmail.com>
2013-05-19 23:53:41 +0900

    grunt-contrib-watch 周りの設定

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,76 @@
-jst: {
-  options: {
-    processName: function (name) {
-      return name.match(/((?:mobile|pc)\/.*).html$/)[1];
-    },
-    processContent: function (src) {
-        return src.replace(/(^\s+|\s+$)/gm, '');
-    },
-    amd: true
-  },
-  pc: {
-    files: {
-      'assets/js/jst/pc.js': ['assets/js/templates/pc/*.html']
-    }
-  },
-  mobile: {
-    files: {
-      'assets/js/jst/mobile.js': ['assets/js/templates/mobile/*.html']
-    }
-  }
-}
+module.exports = function (grunt) {
+  [
+    'grunt-contrib-connect',
+    'grunt-contrib-jst',
+    'grunt-contrib-requirejs',
+    'grunt-contrib-watch'
+  ].forEach(function (name) {
+    grunt.loadNpmTasks(name);
+  });
 
-requirejs: {
-  options: {
-    mainConfigFile: 'assets/js/require.config.js',
-    baseUrl: './assets/js',
-    paths: {
-      requirejs: 'vendor/require'
+  grunt.initConfig({
+    jst: {
+      options: {
+        processName: function (name) {
+          return name.match(/((?:mobile|pc)\/.*).html$/)[1];
+        },
+        processContent: function (src) {
+          return src.replace(/(^\s+|\s+$)/gm, '');
+        },
+        amd: true
+      },
+      pc: {
+        files: {
+          'assets/js/jst/pc.js': ['assets/js/templates/pc/*.html']
+        }
+      },
+      mobile: {
+        files: {
+          'assets/js/jst/mobile.js': ['assets/js/templates/mobile/*.html']
+        }
+      }
     },
-    include: ['requirejs']
-  },
-  pc: {
-    options: {
-      out: 'assets/js/dist/pc.min.js',
-      name: 'pc'
+
+    watch: {
+      jst: {
+        files: ['assets/js/templates/**/*.html'],
+        tasks: ['jst']
+      }
+    },
+
+    requirejs: {
+      options: {
+        mainConfigFile: 'assets/js/require.config.js',
+        baseUrl: './assets/js',
+        paths: {
+          requirejs: 'vendor/require-2.1.2'
+        },
+        include: ['requirejs'],
+        optimize: 'none'
+      },
+      pc: {
+        options: {
+          out: 'assets/js/dist/pc.min.js',
+          name: 'pc'
+        }
+      },
+      mobile: {
+        options: {
+          out: 'assets/js/dist/mobile.min.js',
+          name: 'mobile'
+        }
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 8001,
+          base: './'
+        }
+      }
     }
-  },
-  mobile: {
-    options: {
-      out: 'assets/js/dist/mobile.min.js',
-      name: 'mobile'
-    }
-  }
-}
+  });
+
+  grunt.registerTask('default', ['connect', 'watch']);
+};
